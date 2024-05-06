de04df6bd9eb4a52951036bcc9afeda7447105e6
Nitesh.k <nitesh.k@webcodegenie.com>
2024-05-06 17:46:54 +0530

    Initial commit

---
+++ Headers/CredentialValidator.h
@@ -0,0 +1,18 @@
+//
+//  CredentialValidator.h
+//  CredentialValidator
+//
+//  Created by surendra pratap on 06/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CredentialValidator.
+FOUNDATION_EXPORT double CredentialValidatorVersionNumber;
+
+//! Project version string for CredentialValidator.
+FOUNDATION_EXPORT const unsigned char CredentialValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CredentialValidator/PublicHeader.h>
+
+
